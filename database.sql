b7b73454443cea78fc19edefa9628d1d1d9658fa
Casper <casfro01@easv365.dk>
2026-02-05 11:23:20 +0100

    database schema

---
+++ database.sql
@@ -0,0 +1,26 @@
+﻿DROP SCHEMA IF EXISTS chat CASCADE
+CREATE SCHEMA IF NOT EXISTS chat;
+
+CREATE TABLE chat.user(
+    id TEXT PRIMARY KEY NOT NULL,
+    username TEXT NOT NULL UNIQUE,
+    passwordhash TEXT NOT NULL,
+    createdAt TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE chat.rooms(
+    id TEXT PRIMARY KEY NOT NULL,
+    chatName TEXT UNIQUE
+);
+
+CREATE TABLE chat.messages(
+    id SERIAL PRIMARY KEY,
+    userId TEXT REFERENCES chat.user(id) ON DELETE CASCADE,
+    roomId TEXT REFERENCES chat.rooms(id) ON DELETE CASCADE,
+    chatMessage TEXT
+);
+
+CREATE TABLE chat.userRooms(
+    userId TEXT REFERENCES chat.user(id) ON DELETE CASCADE,
+    roomId TEXT REFERENCES chat.rooms(id) ON DELETE CASCADE
+);
